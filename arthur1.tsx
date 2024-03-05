9ac80ccb9fe3a2176605938e22202e03cf629347
EzData Solutions <jerome@ezdata-solutions.com>
2024-03-05 22:20:45 +0100

    dev level1

---
+++ arthur1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="arthur1" tilewidth="16" tileheight="16" tilecount="120" columns="30">
+ <image source="gfx/tiles_arthur1.png" trans="000000" width="480" height="64"/>
+</tileset>
